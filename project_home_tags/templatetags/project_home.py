32f17fa5b93a5429842d49f8060e6d7790a10af4
Mike Covington <mfcovington@gmail.com>
2018-11-17 21:04:18 -0800

    Add decorator to silence templatetags if 'PROJECT_HOME_NAMESPACE' is not defined in settings

--- project_home_tags/templatetags/project_home.py
+++ project_home_tags/templatetags/project_home.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from django import template
 from django.conf import settings
 from django.urls import reverse
@@ -14,39 +16,46 @@ def home_url():
         return None
 
 
+def silence_without_namespace(f):
+    @wraps(f)
+    def wrapped(label=None):
+        if not home_namespace:
+            return ''
+        if label:
+            return f(label)
+        else:
+            return f()
+    return wrapped
+
+
 @register.simple_tag
-def project_home_url():
+@silence_without_namespace
+def project_home_url(*args):
     url = home_url()
     if url:
         return url
-    else:
-        return ''
 
 
 @register.simple_tag
+@silence_without_namespace
 def project_home_breadcrumb_bs3(label):
-    if home_namespace:
-        url = home_url()
-        if url:
-            return format_html(
-                '<li><a href="{}">{}</a></li>', url, label)
-        else:
-            return format_html('<li>{}</li>', label)
+    url = home_url()
+    if url:
+        return format_html(
+            '<li><a href="{}">{}</a></li>', url, label)
     else:
-        return ''
+        return format_html('<li>{}</li>', label)
 
 
 @register.simple_tag
+@silence_without_namespace
 def project_home_breadcrumb_bs4(label):
-    if home_namespace:
-        url = home_url()
-        if url:
-            return format_html(
-                '<li class="breadcrumb-item" aria-label="breadcrumb"><a href="{}">{}</a></li>',
-                url, label)
-        else:
-            return format_html(
-                '<li class="breadcrumb-item" aria-label="breadcrumb">{}</li>',
-                label)
+    url = home_url()
+    if url:
+        return format_html(
+            '<li class="breadcrumb-item" aria-label="breadcrumb"><a href="{}">{}</a></li>',
+            url, label)
     else:
-        return ''
+        return format_html(
+            '<li class="breadcrumb-item" aria-label="breadcrumb">{}</li>',
+            label)
