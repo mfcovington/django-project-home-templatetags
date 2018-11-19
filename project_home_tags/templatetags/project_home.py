20e489c56ded4516abfef1f34c0aa68cb7def503
Mike Covington <mfcovington@gmail.com>
2018-11-18 22:11:27 -0800

    Add docstrings

--- project_home_tags/templatetags/project_home.py
+++ project_home_tags/templatetags/project_home.py
@@ -11,6 +11,10 @@ home_namespace = getattr(settings, 'PROJECT_HOME_NAMESPACE', None)
 home_label = getattr(settings, 'PROJECT_HOME_LABEL', 'Home')
 
 def home_url():
+    """Get project's home URL based on settings.PROJECT_HOME_NAMESPACE.
+
+    Returns None if PROJECT_HOME_NAMESPACE is not defined in settings.
+    """
     try:
         return reverse(home_namespace)
     except Exception:
@@ -18,6 +22,20 @@ def home_url():
 
 
 def silence_without_namespace(f):
+    """Decorator to silence template tags if 'PROJECT_HOME_NAMESPACE' is
+    not defined in settings.
+
+    Usage Example:
+        from django import template
+
+
+        register = template.Library()
+
+        @register.simple_tag
+        @silence_without_namespace
+        def a_template_tag(*args):
+            ...
+    """
     @wraps(f)
     def wrapped(label=None):
         if not home_namespace:
@@ -32,6 +50,17 @@ def silence_without_namespace(f):
 @register.simple_tag
 @silence_without_namespace
 def project_home_url(*args):
+    """A template tag to return the project's home URL.
+
+    PROJECT_HOME_NAMESPACE must be defined in settings.
+    For example:
+        PROJECT_HOME_NAMESPACE = 'project_name:index_view'
+
+    Usage Example:
+        {% load project_home_tags %}
+
+        <a href="{% project_home_url %}">Home</a>
+    """
     url = home_url()
     if url:
         return url
@@ -40,6 +69,36 @@ def project_home_url(*args):
 @register.simple_tag
 @silence_without_namespace
 def project_home_breadcrumb_bs3(label):
+    """A template tag to return the project's home URL and label
+    formatted as a Bootstrap 3 breadcrumb.
+
+    PROJECT_HOME_NAMESPACE must be defined in settings, for example:
+        PROJECT_HOME_NAMESPACE = 'project_name:index_view'
+
+    Usage Example:
+        {% load project_home_tags %}
+
+        <ol class="breadcrumb">
+          {% project_home_breadcrumb_bs3 %}    {# <--- #}
+          <li><a href="{% url 'app:namespace' %}">List of Objects</a></li>
+          <li class="active">Object Detail</li>
+        </ol>
+
+    This gets converted into:
+        <ol class="breadcrumb">
+          <li><a href="{% url 'project_name:index_view' %}">Home</a></li>    {# <--- #}
+          <li><a href="{% url 'app:namespace' %}">List of Objects</a></li>
+          <li class="active">Object Detail</li>
+        </ol>
+
+    By default, the link's text is 'Home'. A project-wide label can be
+    defined with PROJECT_HOME_LABEL in settings. Both the default and
+    the project-wide label can be overridden by passing a string to
+    the template tag.
+
+    For example:
+        {% project_home_breadcrumb_bs3 'Custom Label' %}
+    """
     url = home_url()
     if url:
         return format_html(
@@ -51,6 +110,36 @@ def project_home_breadcrumb_bs3(label):
 @register.simple_tag
 @silence_without_namespace
 def project_home_breadcrumb_bs4(label):
+    """A template tag to return the project's home URL and label
+    formatted as a Bootstrap 4 breadcrumb.
+
+    PROJECT_HOME_NAMESPACE must be defined in settings, for example:
+        PROJECT_HOME_NAMESPACE = 'project_name:index_view'
+
+    Usage Example:
+        {% load project_home_tags %}
+
+        <ol class="breadcrumb">
+          {% project_home_breadcrumb_bs4 %}    {# <--- #}
+          <li class="breadcrumb-item" aria-label="breadcrumb"><a href="{% url 'app:namespace' %}">List of Objects</a></li>
+          <li class=" breadcrumb-item active" aria-label="breadcrumb" aria-current="page">Object Detail</li>
+        </ol>
+
+    This gets converted into:
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item" aria-label="breadcrumb"><a href="{% url 'project_name:index_view' %}">Home</a></li>    {# <--- #}
+          <li class="breadcrumb-item" aria-label="breadcrumb"><a href="{% url 'app:namespace' %}">List of Objects</a></li>
+          <li class=" breadcrumb-item active" aria-label="breadcrumb" aria-current="page">Object Detail</li>
+        </ol>
+
+    By default, the link's text is 'Home'. A project-wide label can be
+    defined with PROJECT_HOME_LABEL in settings. Both the default and
+    the project-wide label can be overridden by passing a string to
+    the template tag.
+
+    For example:
+        {% project_home_breadcrumb_bs4 'Custom Label' %}
+    """
     url = home_url()
     if url:
         return format_html(
